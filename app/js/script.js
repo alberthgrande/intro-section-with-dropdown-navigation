55e5c3484adeee797a1ee96b359e34efba12b332
Alberth G. Ruado <alberthruado@gmail.com>
2023-03-05 03:19:46 +0800

    done navigation desktop design

--- app/js/script.js
+++ app/js/script.js
@@ -1,2 +1,20 @@
 const toggleMenu = document.querySelector(".btn-mobile-nav");
 const navList = document.querySelectorAll(".nav-list");
+
+const features = document.querySelector(".features");
+const company = document.querySelector(".company");
+
+// sublinks
+const subLinkFeatures = document.querySelector(".nav-link__features");
+const subLinkCompany = document.querySelector(".nav-link__company");
+
+features.addEventListener("click", function () {
+  subLinkFeatures.classList.toggle("active-features");
+  features.classList.toggle("features-up");
+  console.log(subLinkFeatures);
+});
+
+company.addEventListener("click", function () {
+  subLinkCompany.classList.toggle("active-company");
+  company.classList.toggle("company-up");
+});
