18cb52eab6edb99a573af74d98f90386977c487b
Alberth G. Ruado <alberthruado@gmail.com>
2023-03-05 14:10:59 +0800

    Done mobile navigation menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,20 +1,29 @@
 const toggleMenu = document.querySelector(".btn-mobile-nav");
-const navList = document.querySelectorAll(".nav-list");
-
-const features = document.querySelector(".features");
-const company = document.querySelector(".company");
+const container = document.querySelector(".container");
+const overlay = document.querySelector(".overlay");
 
 // sublinks
 const subLinkFeatures = document.querySelector(".nav-link__features");
 const subLinkCompany = document.querySelector(".nav-link__company");
+const features = document.querySelector(".features");
+const company = document.querySelector(".company");
+
+toggleMenu.addEventListener("click", function () {
+  container.classList.toggle("nav-open");
+  overlay.classList.toggle("hidden");
+});
 
 features.addEventListener("click", function () {
   subLinkFeatures.classList.toggle("active-features");
   features.classList.toggle("features-up");
-  console.log(subLinkFeatures);
 });
 
 company.addEventListener("click", function () {
   subLinkCompany.classList.toggle("active-company");
   company.classList.toggle("company-up");
+});
+
+overlay.addEventListener("click", () => {
+  overlay.classList.toggle("hidden");
+  container.classList.remove("nav-open");
 });
